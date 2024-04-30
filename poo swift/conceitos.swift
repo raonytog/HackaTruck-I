43b07b746e70550676057bc6800e52dbf7c619e6
raonytog <raony.gomes@edu.ufes.br>
2024-04-30 16:05:15 -0300

    att

--- poo swift/conceitos.swift
+++ poo swift/conceitos.swift
@@ -38,10 +38,38 @@ class Correntista {
 var pessoa = Correntista() // criando um objeto da classe
 pessoa.saldo = 2.50        // este acesso da erro por causa do 'private'
 
+
 /*********** heranca ***********/
 /* sao as caracteristicas em comuns, como, por exemplo, a classe carro, moto e aviao possuem,
     em comum, a classe 'motor' nelas'
 */ 
 
+
 /*********** polimorfismo ***********/
 /* sao as caracteristicas em comum (heranca), mas com algumas diferencas */
+
+
+/*********** encapsulamento ***********/
+/* sao ferramentas que limitam o acesso de alguns objetos
+   em swift, tem 3:
+   * public: qlqr um pode acessar
+   * internal: apenas acesso na propria classe e nas hereditarias (padrao)
+   * private: apenas a propria classe pode acessar os atributos do objeto
+*/
+class Carro {
+    var modelo: String = "Gool"
+    
+    //Criamos a propriedade privada
+    private var qtdeCombustivel: Float = 25
+    
+    func quantidadeCombustivel()->Void{
+        print(qtdeCombustivel)
+    }
+}
+
+//Criamos o objeto da classe
+var carro1 = Carro()
+
+//Tenta fazer a alteração da qtdeCombustivel, atribuindo diretamente no atributo qtdeCombustivel       
+carro1.qtdeCombustivel = 35
+//Resultado: erro
